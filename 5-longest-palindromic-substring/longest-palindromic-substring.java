e680317b8097a06db71e4b8d5fd45e65c389461b
Rishabh Singh <rishabhsingh.3021@gmail.com>
2026-04-13 23:14:59 +0530

    Time: 114 ms (26.54%) | Memory: 49.3 MB (5.37%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean dp[][] = new boolean [n][n];
+        String r = "";
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s.charAt(i) == s.charAt(j) && (j-i<3 || dp[i+1][j-1])){
+                    dp[i][j]= true;
+                    if(j-i+1>r.length()){
+                        r=s.substring(i,j+1);
+                    }
+                }
+            }
+        }
+        return r;
+    }
+}
